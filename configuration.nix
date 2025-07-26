497da173ef4a7e9863e6899f4d75706b04ba3854
Sergei Andronov <sergei@andronov.me>
2025-07-26 12:32:57 +0200

    21 current  2025-07-26 12:32:55  25.05.807027.3ff0e34b1383  6.12.39                          *

--- configuration.nix
+++ configuration.nix
@@ -123,6 +123,7 @@ in
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     #vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    bun
     direnv
     git
     gh
